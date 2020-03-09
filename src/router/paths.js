423932173967212d5e548b7932c2ab985c2942d1
Jose Gomez <jose@current360.com>
2020-03-09 11:53:58 -0400

    First Push
    Template implementation.
    Client View Partial Setup.

---
+++ src/router/paths.js
@@ -0,0 +1,57 @@
+/**
+ * Define all of your application routes here
+ * for more information on routes, see the
+ * official documentation https://router.vuejs.org/en/
+ */
+export default [
+  {
+    path: '',
+    // Relative to /src/views
+    view: 'Dashboard'
+  },
+  // Clients
+  {
+    path: '/clients-list',
+    name: 'Clients List',
+    view: 'Clients/ClientsList'
+  },
+  {
+    path: '/add-client',
+    name: 'Clients',
+    view: 'Clients/AddClient'
+  },
+  // Jobs
+  // Tasks
+  // Users
+  {
+    path: '/user-profile',
+    name: 'User Profile',
+    view: 'UserProfile'
+  },
+  {
+    path: '/table-list',
+    name: 'Table List',
+    view: 'TableList'
+  },
+  {
+    path: '/typography',
+    view: 'Typography'
+  },
+  {
+    path: '/icons',
+    view: 'Icons'
+  },
+  {
+    path: '/maps',
+    view: 'Maps'
+  },
+  {
+    path: '/notifications',
+    view: 'Notifications'
+  },
+  {
+    path: '/upgrade',
+    name: 'Upgrade to PRO',
+    view: 'Upgrade'
+  }
+]
